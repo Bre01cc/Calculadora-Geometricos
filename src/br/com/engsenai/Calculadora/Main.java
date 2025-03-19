6b754b8747e29359f9a0e65914bd492f5f495fa9
Bre01cc <boliveiraassisreis@gmail.com>
2025-03-19 06:20:03 -0300

    Quadrado

---
+++ src/br/com/engsenai/Calculadora/Main.java
@@ -0,0 +1,15 @@
+package br.com.engsenai.Calculadora;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub {
+		
+		 	Quadrado quadrado1 = new Quadrado();
+		 	quadrado1.setLado(8);
+	 		quadrado1.calcularArea();
+	 		quadrado1.calcularPerimetro();
+	 		quadrado1.mostrarDados();
+		 	
+	}
+}
---
+++ src/br/com/engsenai/Calculadora/Quadrado.java
@@ -0,0 +1,29 @@
+package br.com.engsenai.Calculadora;
+
+	public class Quadrado {
+		private double lado;
+		public double area;
+		public double perimetro;
+	public void setLado(double lado) {
+		this.lado= lado;
+	}
+	public void calcularPerimetro() {
+		perimetro = lado * 4;
+	}
+	public void calcularArea() {
+		area=(lado*lado);
+	}
+	public void mostrarDados() {
+ 		System.out.println("--------------");
+ 		System.out.println("Forma geométrica: Quadrado");
+ 		System.out.println("--------------");
+ 		System.out.println("MEDIDAS");
+ 		System.out.println("lado: "+lado);
+ 		System.out.println("--------------");
+ 		System.out.println("RESOLUÇÕES OBTIDAS");
+		System.out.println("Área: "+ area);
+ 		System.out.println("Perimetro: "+ perimetro);
+ 		System.out.println("--------------");
+	}
+
+}
